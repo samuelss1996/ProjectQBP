598f6c45fad83699c8a137b01daecd59449b445a
travisko <marek.cern@gmail.com>
2019-10-19 16:55:47 +0200

    Added camera rotation around the player. Added screen selector implementataion.

---
+++ Assets/Scripts/Camera/CameraSelector.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSelector : MonoBehaviour
+{
+    public GameObject[] cameras;
+    private bool _screenSelectorEnabled = false;
+    
+    public bool isScreenSelectorEnabled
+    {
+        get { return _screenSelectorEnabled; }
+    }
+
+    void Start()
+    {
+        cameras[0].SetActive(true);
+        Cursor.visible = false;
+    }
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!_screenSelectorEnabled)
+            {
+                _screenSelectorEnabled = true;
+                int modulo = cameras.Length % 2;
+                for (int i = 0; i < cameras.Length; i++)
+                {
+                    ThirdPersonCamera camera = cameras[i].GetComponent<ThirdPersonCamera>();
+                    if (modulo == 0)
+                    {
+                        camera.ResizeViewport(i / (float)cameras.Length, (2 / cameras.Length) == 1 ? 0.0f : 0.5f, 0.5f, 2.0f / (float)cameras.Length);
+                    }
+                    else
+                    {
+                        if (cameras.Length == 3)
+                        {
+                            if (i == 0)
+                            {
+                                camera.ResizeViewport(0.0f, 0.5f, 0.5f, 0.5f);
+                            }
+                            if (i == 1)
+                            {
+                                camera.ResizeViewport(0.5f, 0.5f, 0.5f, 0.5f);
+                            }
+                            if (i == 2)
+                            {
+                                camera.ResizeViewport(0.25f, 0.0f, 0.5f, 0.5f);
+                            }
+                        }
+                        // 5, which is max
+                        else
+                        {
+                            if (i == 0)
+                            {
+                                camera.ResizeViewport(0.0f, 0.66f, 0.5f, 0.33f);
+                            }
+                            if (i == 1)
+                            {
+                                camera.ResizeViewport(0.5f, 0.66f, 0.5f, 0.33f);
+                            }
+                            if (i == 2)
+                            {
+                                camera.ResizeViewport(0.25f, 0.33f, 0.5f, 0.33f);
+                            }
+                            if (i == 3)
+                            {
+                                camera.ResizeViewport(0.0f, 0.0f, 0.5f, 0.33f);
+                            }
+                            if (i == 4)
+                            {
+                                camera.ResizeViewport(0.5f, 0.0f, 0.5f, 0.33f);
+                            }
+                        }
+                    }
+                    cameras[i].SetActive(true);
+                }
+                Cursor.visible = true;
+            }
+        }
+
+        if (_screenSelectorEnabled)
+        {
+            ShowScreenSelector();
+        }
+    }
+
+    private void ShowScreenSelector()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int indexOfselected = -1;
+            float positionX = Input.mousePosition.x / Screen.width;
+            float positionY = Input.mousePosition.y / Screen.height;
+
+            if (cameras.Length == 2)
+            {
+                if (positionX > 0.5f)
+                {
+                    indexOfselected = 1;
+                }
+                else
+                {
+                    indexOfselected = 0;
+                }
+            }
+            if (cameras.Length == 3)
+            {
+                if (positionY > 0.5)
+                {
+                    if (positionX > 0.5f)
+                    {
+                        indexOfselected = 1;
+                    }
+                    else
+                    {
+                        indexOfselected = 0;
+                    }
+                }
+                else
+                {
+                    if (positionX > 0.25f && positionX < 0.75f)
+                    {
+                        indexOfselected = 2;
+                    }
+                }
+            }
+            if (cameras.Length == 4)
+            {
+                if (positionY > 0.5)
+                {
+                    if (positionX > 0.5f)
+                    {
+                        indexOfselected = 1;
+                    }
+                    else
+                    {
+                        indexOfselected = 0;
+                    }
+                }
+                else
+                {
+                    if (positionX < 0.5f)
+                    {
+                        indexOfselected = 2;
+                    }
+                    else
+                    {
+                        indexOfselected = 3;
+                    }
+                }
+            }
+            if (cameras.Length == 5)
+            {
+                if (positionY > 0.66f)
+                {
+                    if (positionX > 0.5f)
+                    {
+                        indexOfselected = 1;
+                    }
+                    else
+                    {
+                        indexOfselected = 0;
+                    }
+                }
+                else if (positionY > 0.33f)
+                {
+                    if (positionX > 0.33f && positionX < 0.66f)
+                    {
+                        indexOfselected = 2;
+                    }
+                }
+                else
+                {
+                    if (positionX > 0.5f)
+                    {
+                        indexOfselected = 4;
+                    }
+                    else
+                    {
+                        indexOfselected = 3;
+                    }
+                }
+            }
+
+            if (indexOfselected >= 0)
+            {
+                for (int i = 0; i < cameras.Length; i++)
+                {
+                    ThirdPersonCamera tpCamera = cameras[i].GetComponent<ThirdPersonCamera>();
+                    tpCamera.ResizeViewport(0.0f, 0.0f, 1.0f, 1.0f);
+                    if (i != indexOfselected)
+                    {
+                        cameras[i].SetActive(false);
+                    }
+                }
+                Cursor.visible = false;
+                _screenSelectorEnabled = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -4,17 +4,90 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public GameObject target;
-    private float _offset;
+    private CameraSelector _cameraSelector;
+    private float _tempDistance = 5.0f;
+    private Vector3 _nonCollidingPosition;
+    private bool _hitting = false;
+
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+    
+    float x = 0.0f;
+    float y = 0.0f;
 
     void Start()
     {
-        _offset = transform.position.z - target.transform.position.z;
-        Cursor.visible = false;
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+        _tempDistance = distance;
+        _nonCollidingPosition = transform.position;
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // Make the rigid body not change rotation
+        if (rigidbody != null)
+        {
+            rigidbody.freezeRotation = true;
+        }
+        _cameraSelector = FindObjectOfType<CameraSelector>();
     }
     
     void LateUpdate()
     {
-        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z + _offset);
+        if (target && !_cameraSelector.isScreenSelectorEnabled)
+        {
+
+            x += Input.GetAxis("Mouse X") * xSpeed * _tempDistance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            _tempDistance = Mathf.Clamp(_tempDistance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+
+            RaycastHit hit;
+            if (Physics.Linecast(target.position, _nonCollidingPosition, out hit))
+            {
+                if (!_hitting)
+                {
+                    _tempDistance -= hit.distance;
+                    _hitting = true;
+                }
+            }
+            else
+            {
+                _hitting = false;
+                _tempDistance = distance;
+            }
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -_tempDistance);
+            Vector3 position = rotation * negDistance + target.position;
+            _nonCollidingPosition = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    public void ResizeViewport(float x, float y, float w, float h)
+    {
+        Camera camera = GetComponent<Camera>();
+        camera.rect = new Rect(x, y, w, h);
     }
 }
