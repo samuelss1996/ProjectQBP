5625082195d39825981623392ca0b6f34617dccf
travisko <marek.cern@gmail.com>
2019-10-19 20:13:30 +0200

    Implemented split/merge logic.

--- Assets/Scripts/Camera/CameraSelector.cs
+++ Assets/Scripts/Camera/CameraSelector.cs
@@ -1,84 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CameraSelector : MonoBehaviour
 {
-    public GameObject[] cameras;
+    private List<ThirdPersonCamera> _cameras = new List<ThirdPersonCamera>();
     private bool _screenSelectorEnabled = false;
-    
+    private ThirdPersonCamera _lastActiveCamera = null;
+
     public bool isScreenSelectorEnabled
     {
         get { return _screenSelectorEnabled; }
     }
 
-    void Start()
+    public ThirdPersonCamera lastActiveCamera
     {
-        cameras[0].SetActive(true);
-        Cursor.visible = false;
+        get { return _lastActiveCamera; }
     }
-   
-    void Update()
+
+    public void EnableSelector()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if ( _cameras.Count > 1)
         {
-            if (!_screenSelectorEnabled)
+            _screenSelectorEnabled = true;
+            int modulo = _cameras.Count % 2;
+            for (int i = 0; i < _cameras.Count; i++)
             {
-                _screenSelectorEnabled = true;
-                int modulo = cameras.Length % 2;
-                for (int i = 0; i < cameras.Length; i++)
+                ThirdPersonCamera camera = _cameras[i].GetComponent<ThirdPersonCamera>();
+                if (modulo == 0)
                 {
-                    ThirdPersonCamera camera = cameras[i].GetComponent<ThirdPersonCamera>();
-                    if (modulo == 0)
+                    camera.ResizeViewport(i / (float)_cameras.Count, (2 / _cameras.Count) == 1 ? 0.0f : 0.5f, 0.5f, 2.0f / (float)_cameras.Count);
+                }
+                else
+                {
+                    if (_cameras.Count == 3)
                     {
-                        camera.ResizeViewport(i / (float)cameras.Length, (2 / cameras.Length) == 1 ? 0.0f : 0.5f, 0.5f, 2.0f / (float)cameras.Length);
+                        if (i == 0)
+                        {
+                            camera.ResizeViewport(0.0f, 0.5f, 0.5f, 0.5f);
+                        }
+                        if (i == 1)
+                        {
+                            camera.ResizeViewport(0.5f, 0.5f, 0.5f, 0.5f);
+                        }
+                        if (i == 2)
+                        {
+                            camera.ResizeViewport(0.25f, 0.0f, 0.5f, 0.5f);
+                        }
                     }
+                    // 5, which is max
                     else
                     {
-                        if (cameras.Length == 3)
+                        if (i == 0)
                         {
-                            if (i == 0)
-                            {
-                                camera.ResizeViewport(0.0f, 0.5f, 0.5f, 0.5f);
-                            }
-                            if (i == 1)
-                            {
-                                camera.ResizeViewport(0.5f, 0.5f, 0.5f, 0.5f);
-                            }
-                            if (i == 2)
-                            {
-                                camera.ResizeViewport(0.25f, 0.0f, 0.5f, 0.5f);
-                            }
+                            camera.ResizeViewport(0.0f, 0.66f, 0.5f, 0.33f);
                         }
-                        // 5, which is max
-                        else
+                        if (i == 1)
                         {
-                            if (i == 0)
-                            {
-                                camera.ResizeViewport(0.0f, 0.66f, 0.5f, 0.33f);
-                            }
-                            if (i == 1)
-                            {
-                                camera.ResizeViewport(0.5f, 0.66f, 0.5f, 0.33f);
-                            }
-                            if (i == 2)
-                            {
-                                camera.ResizeViewport(0.25f, 0.33f, 0.5f, 0.33f);
-                            }
-                            if (i == 3)
-                            {
-                                camera.ResizeViewport(0.0f, 0.0f, 0.5f, 0.33f);
-                            }
-                            if (i == 4)
-                            {
-                                camera.ResizeViewport(0.5f, 0.0f, 0.5f, 0.33f);
-                            }
+                            camera.ResizeViewport(0.5f, 0.66f, 0.5f, 0.33f);
+                        }
+                        if (i == 2)
+                        {
+                            camera.ResizeViewport(0.25f, 0.33f, 0.5f, 0.33f);
+                        }
+                        if (i == 3)
+                        {
+                            camera.ResizeViewport(0.0f, 0.0f, 0.5f, 0.33f);
+                        }
+                        if (i == 4)
+                        {
+                            camera.ResizeViewport(0.5f, 0.0f, 0.5f, 0.33f);
                         }
                     }
-                    cameras[i].SetActive(true);
                 }
-                Cursor.visible = true;
+                _cameras[i].gameObject.SetActive(true);
             }
+            Cursor.visible = true;
+        }
+    }
+
+    void Start()
+    {
+        ThirdPersonCamera mainCamera = FindObjectOfType<ThirdPersonCamera>();
+        _cameras.Add(mainCamera);
+        _lastActiveCamera = mainCamera;
+        Cursor.visible = false;
+    }
+
+    public void AddCamera(ThirdPersonCamera camera)
+    {
+        _cameras.Add(camera);
+    }
+
+    public bool IsActive(ThirdPersonCamera camera)
+    {
+        foreach (ThirdPersonCamera activeCamera in _cameras)
+        {
+            if (activeCamera == camera)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void RemoveDisabledCameras()
+    {
+        foreach (ThirdPersonCamera camera in _cameras)
+        {
+            if (!camera.gameObject.activeInHierarchy)
+            {
+                camera.target.gameObject.SetActive(false);
+            }
+        }
+        _cameras.RemoveAll(camera => !camera.gameObject.activeInHierarchy);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && !_screenSelectorEnabled)
+        {
+            EnableSelector();
         }
 
         if (_screenSelectorEnabled)
@@ -95,7 +138,7 @@ public class CameraSelector : MonoBehaviour
             float positionX = Input.mousePosition.x / Screen.width;
             float positionY = Input.mousePosition.y / Screen.height;
 
-            if (cameras.Length == 2)
+            if (_cameras.Count == 2)
             {
                 if (positionX > 0.5f)
                 {
@@ -106,7 +149,7 @@ public class CameraSelector : MonoBehaviour
                     indexOfselected = 0;
                 }
             }
-            if (cameras.Length == 3)
+            if (_cameras.Count == 3)
             {
                 if (positionY > 0.5)
                 {
@@ -127,7 +170,7 @@ public class CameraSelector : MonoBehaviour
                     }
                 }
             }
-            if (cameras.Length == 4)
+            if (_cameras.Count == 4)
             {
                 if (positionY > 0.5)
                 {
@@ -152,7 +195,7 @@ public class CameraSelector : MonoBehaviour
                     }
                 }
             }
-            if (cameras.Length == 5)
+            if (_cameras.Count == 5)
             {
                 if (positionY > 0.66f)
                 {
@@ -187,13 +230,17 @@ public class CameraSelector : MonoBehaviour
 
             if (indexOfselected >= 0)
             {
-                for (int i = 0; i < cameras.Length; i++)
+                for (int i = 0; i < _cameras.Count; i++)
                 {
-                    ThirdPersonCamera tpCamera = cameras[i].GetComponent<ThirdPersonCamera>();
+                    ThirdPersonCamera tpCamera = _cameras[i].GetComponent<ThirdPersonCamera>();
                     tpCamera.ResizeViewport(0.0f, 0.0f, 1.0f, 1.0f);
                     if (i != indexOfselected)
                     {
-                        cameras[i].SetActive(false);
+                        _cameras[i].gameObject.SetActive(false);
+                    }
+                    else
+                    {
+                        _lastActiveCamera = _cameras[i];
                     }
                 }
                 Cursor.visible = false;
