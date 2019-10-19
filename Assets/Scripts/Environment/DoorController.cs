3b0eb815a1a5e6a410348d3221d6afa63863418f
travisko <marek.cern@gmail.com>
2019-10-19 21:50:40 +0200

    Added mass-based collider. Added door controller.

---
+++ Assets/Scripts/Environment/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 2.0f;
+
+    private Vector3 _basePosition = Vector3.zero;
+    private bool _open = false;
+    private Rigidbody _rigidbody = null;
+    private float _height = 0.0f;
+    
+    public bool open
+    {
+        set { _open = value; }
+    }
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _basePosition = transform.position;
+        Collider collider = GetComponent<Collider>();
+        if (collider)
+        {
+            _height = collider.bounds.size.y;
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        if (_open && (_rigidbody.position.y < _height))
+        {
+            _rigidbody.MovePosition(_rigidbody.position + Vector3.up * movementSpeed * Time.fixedDeltaTime);
+        }
+        else if(_rigidbody.position.y > _basePosition.y)
+        {
+            _rigidbody.MovePosition(_rigidbody.position + Vector3.down * movementSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/DoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private bool _oneTimePress = true;
+    [SerializeField]
+    private float weightThreshold = 0.6f;
+
+    private DoorController _doorController = null;
+
+    private void Start()
+    {
+        _doorController = GetComponentInParent<DoorController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+        if (rigidbody && (weightThreshold <= rigidbody.mass))
+        {
+            _doorController.open = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Rigidbody rigidbody = other.GetComponent<Rigidbody>();
+        if (rigidbody && !_oneTimePress)
+        {
+            _doorController.open = false;
+        }
+    }
+}
