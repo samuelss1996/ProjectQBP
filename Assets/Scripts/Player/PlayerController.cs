560225b6556318847d57e2e3ed9b84acabcbcb57
travisko <marek.cern@gmail.com>
2019-10-19 13:46:31 +0200

    Adding character movement controller.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _movementSpeed;
+
+    private Vector3 _input;
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        _input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _input * _movementSpeed * Time.fixedDeltaTime);
+    }
+}
