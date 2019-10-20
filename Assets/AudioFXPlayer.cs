f8e172b6d90dfef57c3ff0656da46148a9a89602
Samuel <samuelsoutullosobral@gmail.com>
2019-10-20 05:23:34 +0200

    Added music and opening door SFX

---
+++ Assets/AudioFXPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFXPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip openDoor;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+}
--- Assets/Scripts/Environment/DoorController.cs
+++ Assets/Scripts/Environment/DoorController.cs
@@ -9,10 +9,13 @@ public class DoorController : MonoBehaviour
     [SerializeField]
     private int numberOfTriggers = 1;
 
+    private AudioFXPlayer audioFxPlayer;
     private Vector3 _basePosition = Vector3.zero;
     private Rigidbody _rigidbody = null;
     private float _height = 0.0f;
     private int _numberOfTriggersActivated = 0;
+
+    private bool soundPlayed;
 
     public int numOfTriggers
     {
@@ -34,6 +37,8 @@ public class DoorController : MonoBehaviour
         {
             _height = collider.bounds.size.y;
         }
+
+        audioFxPlayer = GameObject.FindWithTag("AudioManager").GetComponent<AudioFXPlayer>();
     }
     
     private void FixedUpdate()
@@ -41,12 +46,23 @@ public class DoorController : MonoBehaviour
         if ((_numberOfTriggersActivated == numberOfTriggers) && (_rigidbody.position.y > -_height))
         {
             _rigidbody.MovePosition(_rigidbody.position + Vector3.up * movementSpeed * Time.fixedDeltaTime);
-
+            PlayDoorOpenOnlyOnce();
         }
         else if ((_numberOfTriggersActivated < numberOfTriggers) && (_rigidbody.position.y < _basePosition.y))
         {
             //_rigidbody.MovePosition(_rigidbody.position + Vector3.down * movementSpeed * Time.fixedDeltaTime);
             _rigidbody.position = _basePosition;
+            soundPlayed = false;
         }
+    }
+
+    private void PlayDoorOpenOnlyOnce()
+    {
+        if(!soundPlayed)
+        {
+            audioFxPlayer.audioSource.PlayOneShot(audioFxPlayer.openDoor);
+        }
+
+        soundPlayed = true;
     }
 }
